afd898a6bc1d5173656ced4bce4b4299716b78ed
jakblak <schroe9@gmail.com>
2024-03-31 10:06:16 -0700

    organizations

--- convex/files.ts
+++ convex/files.ts
@@ -1,20 +1,43 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const createFile = mutation({
   args: {
-    name: v.string()
+    name: v.string(),
+    orgId: v.string(),
   },
   async handler(ctx, args) {
+    // const identity = await ctx.auth.getUserIdentity();
+    // if(!identity) { 
+    //   throw new ConvexError('must be logged in')
+    // }
     await ctx.db.insert('files', {
-      name: args.name
+      name: args.name,
+      orgId: args.orgId,
     });
   }
 });
 
+export const getFileByOrg = query({
+  args: {
+    orgId: v.string()
+  },
+  async handler(ctx, args) {
+    // const identity = await ctx.auth.getUserIdentity();
+    // if(!identity) { return []; }
+
+    return ctx.db
+    .query('files')
+    .withIndex('by_orgId', (q) => q.eq('orgId', args.orgId))
+    .collect();
+  }
+})
 export const getFile = query({
   args: {},
   async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+    if(!identity) { return []; }
+
     return ctx.db.query('files').collect();
   }
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { SignedOut } from "@clerk/clerk-react";
-import { SignInButton, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, SignedIn, useOrganization, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const organization = useOrganization();
+  const user = useUser();
+  let orgId: string | undefined = undefined;
+  if (organization.isLoaded && user.isLoaded) {
+    orgId = organization.organization?.id ?? user.user?.id;
+  }
+
   const createFile = useMutation(api.files.createFile);
-  const files = useQuery(api.files.getFile);
+  const files = useQuery(
+    api.files.getFileByOrg, 
+    orgId ? {orgId}: "skip");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <SignedIn>
@@ -26,8 +36,9 @@ export default function Home() {
         }) 
       }
       <Button onClick={()=> {
-        createFile({
+        if (!orgId) return; createFile({
           name: 'hello world',
+          orgId,
         });
       }} >
         Click me
