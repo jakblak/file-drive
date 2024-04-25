34bd879fcf4cf7cf9179ecbdf42e19ac16867284
jakblak <schroe9@gmail.com>
2024-04-24 17:00:32 -0700

    form components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { SignedOut } from "@clerk/clerk-react";
-import { SignInButton, SignOutButton, SignedIn, useOrganization, useUser } from "@clerk/nextjs";
+import { useOrganization, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
-import { useConvexAuth } from "convex/react";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z
+    .custom<FileList>((val) => val instanceof FileList, "Required")
+    .refine((files) => files.length > 0, `Required`),
+});
 
 export default function Home() {
   const organization = useOrganization();
@@ -13,38 +25,106 @@ export default function Home() {
   if (organization.isLoaded && user.isLoaded) {
     orgId = organization.organization?.id ?? user.user?.id;
   }
-  const { isLoading, isAuthenticated } = useConvexAuth();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  });
+  const [isFileDialogOpen, setIsFileDialogOpen] = useState(false);
+  const fileRef = form.register("file");
   const createFile = useMutation(api.files.createFile);
   const files = useQuery(
     api.files.getFileByOrg, 
     orgId ? {orgId}: "skip");
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <SignedIn>
-        <SignOutButton>
-          <Button>Sign out</Button>
-        </SignOutButton>
-      </SignedIn>
-      <SignedOut>
-       <SignInButton mode="modal">
-       <Button>Sign in</Button>
-       </SignInButton>
-      </SignedOut>
+    <main className="container mx-auto p-12">
+     <div className="flex justify-between items-center">
+       <h1 className="text-txl font-bold">Your files</h1>
+       <Dialog
+        open={isFileDialogOpen}
+        onOpenChange={(isOpen) => {
+         setIsFileDialogOpen(isOpen);
+        form.reset();
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button>Upload File</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="mb-8">Upload your File Here</DialogTitle>
+          <DialogDescription>
+            This file will be accessible by anyone in your organization
+          </DialogDescription>
+        </DialogHeader>
 
-      {files?.map((file) => {
-        return <div key={file._id}>{file.name}</div>
-        }) 
-      }
-      <Button onClick={()=> {
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="file"
+                render={() => (
+                  <FormItem>
+                    <FormLabel>File</FormLabel>
+                    <FormControl>
+                      <Input type="file" {...fileRef} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                disabled={form.formState.isSubmitting}
+                className="flex gap-1"
+              >
+                {/* {form.formState.isSubmitting && (
+                  <Loader2 className="h-4 w-4 animate-spin" />
+                )} */}
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </DialogContent>
+    </Dialog>
+      {/* <Button onClick={()=> {
         if (!orgId) return; createFile({
           name: 'hello world',
           orgId,
         });
       }} >
-        Click me
-      </Button>
+        Upload file
+      </Button> */}
+      </div>
+
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>
+        }) 
+      }
       
     </main>
   );
+}
+
+function onSubmit(values: z.infer<typeof formSchema>) {
+  console.log(values);
 }
